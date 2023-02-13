20f27059952e0a04812af25aa2c85b9ca11a30b9
kossiericahialey <eric.ahialey@aceko.fr>
2023-02-13 09:42:51 +0100

    Remove React big logo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-    render(<App/>);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
-});
-
 test('should render button', () => {
     render(<App/>);
     const button = screen.getByTestId('testBtn');
--- src/cat-events-list/cat-events-list.test.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import {render, screen} from "@testing-library/react";
-import {CatEventList} from "./cat-event-list";
-import {User} from "../shared/User";
-
-let defaultUsers: User[] = [];
